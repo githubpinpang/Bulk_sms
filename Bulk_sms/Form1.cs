bde19117154d1f3a91b4e9377527d6d6a916dc3b
githubpinpang <48709591+githubpinpang@users.noreply.github.com>
2019-08-30 17:18:11 +0300

    sms-sending_code_only_for_test_phones

---
+++ Bulk_sms/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace Bulk_sms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Test+message+text
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient w = new WebClient();
+                Stream s = w.OpenRead(string.Format("https://platform.clickatell.com/messages/http/send?apiKey=xI3dqDS5QeG4n5lM4HXXPA==&to={0}&content={1}",
+
+                                                    textBox1.Text, textBox2.Text));
+                StreamReader reader = new StreamReader(s);
+                string result = reader.ReadToEnd();
+                MessageBox.Show(result, "message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "message",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
